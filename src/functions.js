90711ad5243e9bf85796a9750ff26d42a14131c6
cyliim <brickmanbots@gmail.com>
2019-06-27 16:26:41 +1200

    Create functions.js

---
+++ src/functions.js
@@ -0,0 +1,6 @@
+{
+    "noargs": "Please provide an input",
+    "nomen": "Please mention a user",
+    "noperms": "You do not have permissions to use this command",
+    "devperms": "This command is a developer only command"
+}
